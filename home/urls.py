68a5c2eca914b687cc9e31c0a27d38aed582b08a
onadj <oliver.nad@gmail.com>
2021-08-02 13:32:06 +0000

    added article details.html

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .import views
+from .views import HomeView, ArticleDetailView
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
-def index(request):
-    return render(request, 'home/index.html')
+#def index(request):
+  #  return render(request, 'home/index.html')#
+
+class HomeView(ListView):
+  model = Post
+  template_name = 'home/index.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+
+
