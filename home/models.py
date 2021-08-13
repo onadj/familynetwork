479d2c2bfd7f94f582b3424667d35c3b67d84408
onadj <oliver.nad@gmail.com>
2021-08-13 08:37:11 +0000

    Add Profile Pic and Social Media Links

--- home/models.py
+++ home/models.py
@@ -19,14 +19,19 @@ class Category(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
-    
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="media/")
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    pinterest_url = models.CharField(max_length=255, null=True, blank=True)
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+   
     def __str__(self):
         return str(self.user)
 
 ########################################################
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to="media/")
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     author.first_name = models.ForeignKey(User, on_delete=models.CASCADE)
