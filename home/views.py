2598995d483c8f71a777ce3a7614031cdba243cd
onadj <oliver.nad@gmail.com>
2021-08-19 10:00:32 +0000

    fixing base.html part 2

--- home/views.py
+++ home/views.py
@@ -24,6 +24,12 @@ class HomeView(ListView):
   template_name = 'home/index.html'
   ordering = ['-post_date']
 
+  def get_context_data(self, *args, **kwargs):
+      cat_menu = Category.objects.all()
+      context = super(HomeView, self).get_context_data(*args, **kwargs)
+      context["cat_menu"] = cat_menu
+      return context
+
 def CategoryView(request, cats):
    category_posts = Post.objects.filter(category=cats.replace('-', ' '))
    return render(request, 'categories.html', {'cats': cats.title().replace('-', ' '), 'category_posts': category_posts})
